30b13b31825ffa021d42df1790a33addbbb7bff0
michbur <michalburdukiewicz@gmail.com>
2018-03-16 14:55:52 +0100

    improved protein selection

--- select_proteins.R
+++ select_proteins.R
@@ -1,10 +1,9 @@
 library(dplyr)
 
-protein_type = "CsgA"
+protein_type <- "CsgA"
+input_file <- "example.fasta"
 
-all_lines <- readLines("example.fasta")
-
-prot_names <- all_lines[grepl("^>", all_lines)] 
+all_lines <- readLines(input_file)
 
 all_prots <- split(all_lines, cumsum(grepl("^>", all_lines)))
 all_prots[sapply(all_prots, function(i)
