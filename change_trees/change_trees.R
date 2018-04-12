954ec3e4df70af5620e403678ffcff2f960cb316
michbur <michalburdukiewicz@gmail.com>
2018-04-12 09:49:18 +0200

    added treechanger

---
+++ change_trees/change_trees.R
@@ -0,0 +1,33 @@
+library(dplyr)
+
+all_lines <- readLines("./change_trees/csgcTREE.aln")
+
+prot_id <- cumsum(grepl("^>", all_lines))
+
+all_prots <- split(all_lines, prot_id)
+
+ids <- sapply(all_prots, first) %>% 
+  strsplit(" ") %>% 
+  sapply(first) %>% 
+  sub(">", "", x = .) %>% 
+  sub("/", "_", x = .)
+
+ids_only <- sapply(all_prots, first) %>% 
+  strsplit("/") %>% 
+  sapply(first) %>% 
+  sub(">", "", x = .)
+
+org_names <- sapply(all_prots, first) %>% 
+  strsplit("[", fixed = TRUE) %>% 
+  sapply(last) %>% 
+  sub("]", "", x = ., fixed = TRUE)
+
+name_df <- data.frame(id = ids, full_name = paste(ids_only, org_names), stringsAsFactors = FALSE)
+
+tree_lines <- readLines("./change_trees/csgcTREE.aln.treefile")
+
+for(i in 1L:nrow(name_df)) {
+  tree_lines <- sub(name_df[i, "id"], name_df[i, "full_name"], tree_lines)
+}
+
+cat(tree_lines, file = "./change_trees/csgcTREE_nice_names.aln.treefile")
