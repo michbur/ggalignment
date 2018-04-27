737c8bed6ff88fddbf743e4cbbd3812cabdd1c75
michbur <michalburdukiewicz@gmail.com>
2018-04-27 14:28:30 +0200

    added tree plotting

---
+++ plot_tree.R
@@ -0,0 +1,10 @@
+library(ggtree)
+
+tr <- read.tree("./change_trees/csgcTREE_nice_names.aln.treefile")
+
+cairo_pdf("tree.pdf", height = 8.6, width = 6.5)
+ggtree(tr) + 
+  geom_treescale() + 
+  geom_tiplab(size = 2) +
+  scale_x_continuous(limits = c(0, 4))
+dev.off()
