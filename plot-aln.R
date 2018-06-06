94b4539deb084054922e95aedf2c4643c5dd78c0
michbur <michalburdukiewicz@gmail.com>
2018-06-07 00:05:05 +0200

    added minus

--- plot-aln.R
+++ plot-aln.R
@@ -55,18 +55,34 @@ aln_dat <- lapply(all_prots, function(ith_prot) {
          pos_prone = ifelse(aa != "-", pos_prone, NA)) %>% 
   mutate(aa_groups_label = letters[as.numeric(aa_groups)])
 
+GeomText[["draw_key"]] <- function(data, params, size) {
+  grid::textGrob("-", 0.5, 0.5, rot = data$angle, 
+                 gp = grid::gpar(col = alpha(data$colour, data$alpha), 
+                                 fontfamily = data$family, 
+                                 fontface = data$fontface, 
+                                 fontsize = data$size * .pt))
+} 
+
+GeomTile[["draw_key"]] <- function(data, params, size) {
+  lwd <- min(data$size, min(size)/4)
+  
+  grid::rectGrob(width = unit(1, "npc") - unit(lwd, "mm"), height = unit(1, "npc") - unit(lwd, "mm"), 
+           gp = grid::gpar(col = "black", fill = alpha(data$fill, data$alpha), 
+                     lty = data$linetype, lwd = lwd * .pt, linejoin = "mitre"))
+} 
+
+
 #cairo_pdf("aln.pdf", height = 4.8, width = 6.5)
-ggplot(aln_dat, aes(x = pos, y = species, label = aa, fill = aa_groups, color = aa_groups_label)) +
+ggplot(aln_dat, aes(x = pos, y = species, label = aa, fill = aa_groups, color = aa_groups)) +
   geom_tile(color = NA) +
   facet_wrap(~ pos_disc, ncol = 1, scales = "free_x") +
   geom_tile(aes(x = pos_prone), color = "black", fill = NA, size = 0.4) +
-  geom_text(size = 2.1) +
-  geom_text(size = 2.1, color = "black") +
+  geom_text(size = 2.1, show.legend = TRUE) +
+  geom_text(size = 2.1, color = "black", show.legend = FALSE) +
   scale_x_continuous("Position", expand = c(0, 0)) +
-  scale_fill_manual(values = cols) +
-  scale_color_manual(values = c(cols[1L:5], "black")) +
-  guides(fill = guide_legend(nrow = 1, byrow = TRUE, override.aes = list(label = "-")),
-         label = guide_legend(nrow = 1, byrow = TRUE)) + 
+  scale_fill_manual("", values = cols) +
+  scale_color_manual("", values = c(cols[1L:5], "black")) +
+  guides(fill = guide_legend(nrow = 1, byrow = TRUE)) + 
   theme_bw(base_size = 7) +
   theme(legend.position = "bottom",
         legend.key.size = unit(0.7, "lines"),
